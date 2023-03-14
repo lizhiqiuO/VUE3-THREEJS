a3a58104f2a18de32a7d3765cd2800a5feac5e88
lizhiqiuO <lizhiqiu0807@163.com>
2023-03-15 00:55:32 +0800

    环境搭建

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="ABC">
+    <HelloWorld />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped="less">
+#ABC {
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    小王，我好困
+    <HeartFilled :style="{ color: 'hotpink' }" />
+    <a-icon type="step-forward" />
+  </div>
+  <a-spin size="large" />
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import { HeartFilled } from '@ant-design/icons-vue'
+</script>
+
+<style scoped="less">
+.test {
+  font-size: 80px;
+}
+</style>
